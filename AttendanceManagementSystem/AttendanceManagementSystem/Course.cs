0d119b39658fd187530d19628b58c440cb36f175
MohamedHelmy12 <kasaby14@gmail.com>
2024-02-16 20:05:50 +0200

    first commit

---
+++ AttendanceManagementSystem/AttendanceManagementSystem/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceManagementSystem
+{
+    internal class Course
+    {
+        public string CourseCode { get; set; }
+        public string CourseName { get; set; }
+        public DateTime Date {  get; set; }
+        public string AttendanceStatus { get; set; }
+
+
+
+        public Course(string code, string name , DateTime date)
+        {
+            CourseCode = code;
+            CourseName = name;
+            Date = date;
+        }
+    }
+}
